a3e53d18990f63573e449adac49c5eb66043dc15
Kamil Handzlik <kamol0209@gmail.com>
2024-05-20 19:22:47 +0200

    added class Tile I have done little bit of reformating and made display draw the grid for game

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pg.init()
 #######################################################################
 # -------------------------Variables/Settins--------------------------#
 #######################################################################
+# -------Window------------
 WIDTH, HEIGHT = 800, 800
 FPS = 60
 ROWS = 4
@@ -16,24 +17,67 @@ RECT_WIDTH = WIDTH // COLS
 
 FONT = pg.font.SysFont("Jacquard 12", 60, bold=True)
 
+# -------Loop-----------------
 run = True
 clock = pg.time.Clock()
 
-# -----Colors--------------
+# ------Colors--------------
 OUTLINE_COLOR = (187, 173, 160)
 OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
 
 
-# -----Window Settings ---------------
+# -------Window Settings ---------------
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("2048 Game")
+pg.display.set_caption("2048 Rip Off Game")
 
 
 ########################################################################
-# ------------------------Classes/Functions-----------------------------#
+# ------------------------Classes--------------------------------------#
 ########################################################################
+class Tile:
+    COLORS = [
+        (237, 229, 218),
+        (238, 225, 201),
+        (243, 178, 122),
+        (246, 150, 101),
+        (247, 124, 95),
+        (247, 95, 59),
+        (237, 208, 115),
+        (237, 204, 99),
+        (236, 202, 80),
+    ]
+
+    def __init__(self, value, row, col) -> None:
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+
+########################################################################
+# ------------------------Functions------------------------------------#
+########################################################################
+
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pg.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pg.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pg.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    draw_grid(window)
+    pg.display.update()
 
 
 ########################################################################
@@ -41,7 +85,7 @@ pg.display.set_caption("2048 Game")
 ########################################################################
 
 
-def main(win):
+def main(window):
     clock.tick(FPS)
     run = True
 
@@ -52,6 +96,7 @@ def main(win):
             if event.type == pg.QUIT:
                 run = False
                 break
+        draw(window)
     pg.quit()
 
 
